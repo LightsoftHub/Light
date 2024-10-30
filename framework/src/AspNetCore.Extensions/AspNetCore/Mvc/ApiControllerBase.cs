d85de2ae202f0f160a71a8c2072c145ea2148b34
minh.vu <minhvu.box@gmail.com>
2024-10-30 19:43:37 +0700

    update

--- framework/src/AspNetCore.Extensions/AspNetCore/Mvc/ApiControllerBase.cs
+++ framework/src/AspNetCore.Extensions/AspNetCore/Mvc/ApiControllerBase.cs
@@ -21,6 +21,19 @@ public abstract class ApiControllerBase : ControllerBase
     /// </summary>
     /// <returns></returns>
     [ApiExplorerSettings(IgnoreApi = true)]
+    public new virtual IActionResult Ok()
+    {
+        var result = Result.Success();
+        result.RequestId = HttpContext.TraceIdentifier;
+
+        return result.ToActionResult();
+    }
+
+    /// <summary>
+    /// Add Trace ID to existing Result model response
+    /// </summary>
+    /// <returns></returns>
+    [ApiExplorerSettings(IgnoreApi = true)]
     public virtual IActionResult Ok(IResult result)
     {
         result.RequestId = HttpContext.TraceIdentifier;
