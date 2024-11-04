53ef520853f425478650a9771a1d136afd6e4234
minh.vu <minhvu.box@gmail.com>
2024-11-04 23:32:40 +0700

    add tests

---
+++ framework/tests/IntegrationTests/TestValues.cs
@@ -0,0 +1,18 @@
+﻿namespace IntegrationTests;
+
+internal abstract class TestValues
+{
+    public const string CreateByUserId = "UserCreate";
+
+    public static DateTimeOffset CreateAtTime = new(new DateTime(2024, 01, 01));
+
+    public const string UpdateByUserId = "UserUpdate";
+
+    public static DateTimeOffset UpdateAtTime = new(new DateTime(2024, 01, 02));
+
+    public const string DeleteByUserId = "UserDelete";
+
+    public static DateTimeOffset DeleteAtTime = new(new DateTime(2024, 01, 03));
+
+    public const string TenandId = "TenantTest";
+}
---
+++ framework/tests/IntegrationTests/Testing.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IntegrationTests;
+
+[SetUpFixture]
+public partial class Testing
+{
+    private static TestDbContext _context;
+
+    [OneTimeSetUp]
+    public void RunBeforeAnyTests()
+    {
+        // Set up the InMemory database options
+        var options = new DbContextOptionsBuilder<TestDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        _context = new TestDbContext(options);
+    }
+
+    public static DbSet<TEntity> Set<TEntity>()
+        where TEntity : class
+    {
+        return _context.Set<TEntity>();
+    }
+
+    public static async Task AddAsync<TEntity>(TEntity entity)
+        where TEntity : class
+    {
+        await _context.Set<TEntity>().AddAsync(entity);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public static async Task RemoveAsync<TEntity>(TEntity entity)
+        where TEntity : class
+    {
+        _context.Set<TEntity>().Remove(entity);
+
+        await _context.SaveChangesAsync();
+    }
+
+    public static async Task SaveAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    [OneTimeTearDown]
+    public async Task RunAfterAnyTests()
+    {
+        // Clean up database after each test
+        await _context.Database.EnsureDeletedAsync();
+        await _context.DisposeAsync();
+    }
+}
