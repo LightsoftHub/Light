cae2589f8541821016613becca143851845a70fb
minh.vu <minhvu.box@gmail.com>
2024-11-12 22:09:52 +0700

    update nuget

---
+++ framework/sample/EventBusConsumer/Common/ColorChangedIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿using Light.EventBus.Events;
+
+namespace EventBusSample.Common
+{
+    [BindingName("color-value-changed")]
+    public record ColorChangedIntegrationEvent : EventBase
+    {
+        public string OldColor { get; set; } = null!;
+
+        public string NewColor { get; set; } = null!;
+
+        public DateTime ChangeOn { get; set; }
+    }
+}
---
+++ framework/sample/EventBusConsumer/Common/IntegrationEvent.cs
@@ -0,0 +1,22 @@
+﻿using Light.Domain;
+using Light.EventBus.Events;
+
+namespace EventBusSample.Common;
+
+public abstract record IntegrationEvent : IIntegrationEvent
+{
+    protected IntegrationEvent()
+    {
+        Id = LightId.NewId();
+        CreationDate = DateTime.UtcNow;
+    }
+
+    public string Id { get; set; }
+
+    public DateTime CreationDate { get; set; }
+}
+
+public abstract record EventBase : IntegrationEvent
+{
+    public int Value = 1000;
+}
