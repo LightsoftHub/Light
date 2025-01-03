a7aecad86247f7dd43ba50ea0d8d07bc078c840c
minh.vu <minhvu.box@gmail.com>
2025-01-03 22:14:13 +0700

    update extensions

--- framework/src/Extensions/EnumHelper.cs
+++ framework/src/Extensions/EnumHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Light.Extensions
@@ -113,6 +114,12 @@ namespace Light.Extensions
             }
 
             return enumValList;
+        }
+
+        public static IEnumerable<T> GetAll<T>()
+            where T : Enum
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
         }
     }
 }
