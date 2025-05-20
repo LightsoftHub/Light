f82d9a5869a38064f19319b0fd5ccf20af8bc9d0
minh.vu <minhvu.box@gmail.com>
2025-05-20 10:24:39 +0700

    Squashed commit of the following:
    commit 57f7fa63713f3ed4c0613147c15c5c89188364cb
    Author: minhvu <84697874+5-fire@users.noreply.github.com>
    Date:   Tue May 20 09:26:34 2025 +0700
    
        Update README.md
    
    commit b616d2fd65095938b1e051b38436414c2952c37c
    Author: minh.vu <minhvu.box@gmail.com>
    Date:   Thu May 15 15:49:15 2025 +0700
    
        update CSV
    
    commit 84d9bf61d64c4efe603a23d4f41e3c92d2a5d12d
    Author: minh.vu <minhvu.box@gmail.com>
    Date:   Wed May 14 10:23:39 2025 +0700
    
        fix AspNetCore
    
    commit 91c87684068e537d9425a7f6df67aee913a19dc0
    Author: minh.vu <minhvu.box@gmail.com>
    Date:   Wed May 14 10:14:53 2025 +0700
    
        add CPVM
    
    commit 7a91192e88ae9680a267225236ddbc95f651b2cb
    Author: minh.vu <minhvu.box@gmail.com>
    Date:   Mon May 12 18:32:09 2025 +0700
    
        add Teams service
    
    commit d9752797041a938c7188f6655097787067480122
    Author: minh.vu <minhvu.box@gmail.com>
    Date:   Mon May 12 18:30:52 2025 +0700
    
        update nuget

--- src/FileGenerator/File/Csv/CsvData.cs
+++ src/FileGenerator/File/Csv/CsvData.cs
@@ -4,7 +4,7 @@ namespace Light.File.Csv
 {
     public class CsvData<T>
     {
-        public string[]? Headers { get; set; }
+        public string[] Headers { get; set; } = null!;
 
         public IEnumerable<T> Rows { get; set; } = new List<T>();
     }
