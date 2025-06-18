9cc7b4ad9902ebc1c3842a58166940903ca78b97
minh.vu <minhvu.box@gmail.com>
2025-06-18 09:42:28 +0700

    add mud blazor

---
+++ src/Blazor/BlazorServer/Program.cs
@@ -0,0 +1,38 @@
+using BlazorServer.Components;
+using Shared;
+
+using Light.MudBlazor;
+using Light.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudBlazorExtraComponents();
+
+builder.Services.AddFileGenerator();
+
+builder.Services.AddSingleton<ProductService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ src/Mediator.Contracts/IQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace Light.Mediator
+{
+    public interface IQuery<TResponse> : IRequest<TResponse>
+    { }
+
+    public interface IQueryHandler<in TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
+        where TRequest : IQuery<TResponse>
+    { }
+}
